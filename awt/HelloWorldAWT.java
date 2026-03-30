a74145820c7d20625545957e34efb854c2f17692
Jevin Sweval <jevinsweval@gmail.com>
2026-03-30 13:28:03 -0400

    hello world

---
+++ awt/HelloWorldAWT.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class HelloWorldAWT {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Hello, World!");
+        Label label = new Label("Hello, World!", Label.CENTER);
+        frame.add(label);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
