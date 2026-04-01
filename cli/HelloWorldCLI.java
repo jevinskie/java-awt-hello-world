b8837a4ab4fa9ddcb640d135314db0ebe22efdfe
Jevin Sweval <jevinsweval@gmail.com>
2026-04-01 11:45:40 -0400

    nit

--- cli/HelloWorldCLI.java
+++ cli/HelloWorldCLI.java
@@ -1,5 +1,6 @@
 public class HelloWorldCLI {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.getProperties().forEach((k, v) -> { System.out.printf("%s: %s\n", k, v); });
     }
 }
