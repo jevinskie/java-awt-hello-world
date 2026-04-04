5f76ec71cd32114fe7614d0fa5f79b8a99cd04ef
Jevin Sweval <jevinsweval@gmail.com>
2026-04-04 15:53:39 -0400

    more headless info

--- awt/HelloWorldAWT.java
+++ awt/HelloWorldAWT.java
@@ -1,4 +1,5 @@
 import java.awt.Frame;
+import java.awt.GraphicsEnvironment;
 import java.awt.Label;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -6,6 +7,13 @@ import java.awt.event.WindowEvent;
 public class HelloWorldAWT {
     public static void main(String[] args) {
         System.getProperties().forEach((k, v) -> { System.out.printf("%s: %s\n", k, v); });
+        System.out.println("");
+
+        System.out.printf("GfxEnv.isHeadless: %b\n", GraphicsEnvironment.isHeadless());
+        System.out.printf("GfxEnv.LclCtx.isHeadlessInstance: %b\n",
+                          GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance());
+        System.out.println("");
+
         Frame frame = new Frame("Hello, World title");
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -18,6 +26,7 @@ public class HelloWorldAWT {
         frame.add(label);
         frame.setSize(300, 200);
         frame.setVisible(true);
+        System.out.println("visible: true");
         Runtime.getRuntime().addShutdownHook(
             new Thread(() -> { System.out.println("shutdown hook"); }));
     }
