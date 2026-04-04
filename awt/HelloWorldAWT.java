674f9a70fc6115cbd31e7e0a5246b79f0565f8fb
Jevin Sweval <jevinsweval@gmail.com>
2026-04-04 11:56:44 -0400

    dump properties in awt

--- awt/HelloWorldAWT.java
+++ awt/HelloWorldAWT.java
@@ -5,6 +5,7 @@ import java.awt.event.WindowEvent;
 
 public class HelloWorldAWT {
     public static void main(String[] args) {
+        System.getProperties().forEach((k, v) -> { System.out.printf("%s: %s\n", k, v); });
         Frame frame = new Frame("Hello, World title");
         frame.addWindowListener(new WindowAdapter() {
             @Override
